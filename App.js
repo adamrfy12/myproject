481ddd84a3e499348ccfe68c54a87adebaf37c0e
adamrfy12 <124908899+adamrfy12@users.noreply.github.com>
2023-02-09 18:59:09 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,27 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import { StyleSheet, View } from 'react-native/types';
+const App = () => {   
+  return <View style={style.container}>
+<View></View>
+  </View>     
+}
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red',  }
+})
+export default App;
